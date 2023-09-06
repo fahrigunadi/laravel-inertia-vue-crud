f5856be40b76c0251492489d486df92d6c2639fd
Fahri Gunadi <fahripx0@gmail.com>
2023-09-06 09:36:03 +0700

    feat: page home

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({ page: String });
+</script>
+
+<template>
+    <div class="text-center pb-5">
+        <h1>{{ page }}</h1>
+
+        <Link class="btn btn-primary mt-3 rounded-pill" href="/users">CRUD User</Link>
+    </div>
+</template>
